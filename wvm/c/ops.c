25aee6254023fdac04bb45ebc694761198f9ae46
Kai Tamkun <ktamkun@ucsc.edu>
2018-04-14 03:28:21 -0700

    Added skeletons to ops.c.

---
+++ wvm/c/ops.c
@@ -0,0 +1,249 @@
+/***********************************************
+ * Contains functions that execute operations. *
+ ***********************************************/
+
+#include "ops.h"
+
+void op_add(lomg instruction) {
+
+}
+
+void op_sub(lomg instruction) {
+
+}
+
+void op_mult(lomg instruction) {
+
+}
+
+void op_addu(lomg instruction) {
+
+}
+
+void op_subu(lomg instruction) {
+
+}
+
+void op_multu(lomg instruction) {
+
+}
+
+void op_sll(lomg instruction) {
+
+}
+
+void op_srl(lomg instruction) {
+
+}
+
+void op_sra(lomg instruction) {
+
+}
+
+void op_and(lomg instruction) {
+
+}
+
+void op_nand(lomg instruction) {
+
+}
+
+void op_nor(lomg instruction) {
+
+}
+
+void op_not(lomg instruction) {
+
+}
+
+void op_or(lomg instruction) {
+
+}
+
+void op_xnor(lomg instruction) {
+
+}
+
+void op_xor(lomg instruction) {
+
+}
+
+void op_addi(lomg instruction) {
+
+}
+
+void op_subi(lomg instruction) {
+
+}
+
+void op_multi(lomg instruction) {
+
+}
+
+void op_addui(lomg instruction) {
+
+}
+
+void op_subui(lomg instruction) {
+
+}
+
+void op_multui(lomg instruction) {
+
+}
+
+void op_slli(lomg instruction) {
+
+}
+
+void op_srli(lomg instruction) {
+
+}
+
+void op_srai(lomg instruction) {
+
+}
+
+void op_andi(lomg instruction) {
+
+}
+
+void op_nandi(lomg instruction) {
+
+}
+
+void op_nori(lomg instruction) {
+
+}
+
+void op_ori(lomg instruction) {
+
+}
+
+void op_xnori(lomg instruction) {
+
+}
+
+void op_xori(lomg instruction) {
+
+}
+
+void op_lui(lomg instruction) {
+
+}
+
+void op_sl(lomg instruction) {
+
+}
+
+void op_sle(lomg instruction) {
+
+}
+
+void op_seq(lomg instruction) {
+
+}
+
+void op_slu(lomg instruction) {
+
+}
+
+void op_sleu(lomg instruction) {
+
+}
+
+void op_sli(lomg instruction) {
+
+}
+
+void op_slei(lomg instruction) {
+
+}
+
+void op_seqi(lomg instruction) {
+
+}
+
+void op_slui(lomg instruction) {
+
+}
+
+void op_sleui(lomg instruction) {
+
+}
+
+void op_j(lomg instruction) {
+
+}
+
+void op_jc(lomg instruction) {
+
+}
+
+void op_jl(lomg instruction) {
+
+}
+
+void op_jlc(lomg instruction) {
+
+}
+
+void op_jr(lomg instruction) {
+
+}
+
+void op_jrc(lomg instruction) {
+
+}
+
+void op_jrl(lomg instruction) {
+
+}
+
+void op_jrlc(lomg instruction) {
+
+}
+
+void op_c(lomg instruction) {
+
+}
+
+void op_l(lomg instruction) {
+
+}
+
+void op_s(lomg instruction) {
+
+}
+
+void op_cb(lomg instruction) {
+
+}
+
+void op_lb(lomg instruction) {
+
+}
+
+void op_sb(lomg instruction) {
+
+}
+
+void op_li(lomg instruction) {
+
+}
+
+void op_si(lomg instruction) {
+
+}
+
+void op_lbi(lomg instruction) {
+
+}
+
+void op_sbi(lomg instruction) {
+
+}
+
+void op_set(lomg instruction) {
+
+}
--- wvm/c/ops.h
+++ wvm/c/ops.h
@@ -1,77 +1,79 @@
 #ifndef __OPS_H__
 #define __OPS_H__
 
+#include "wvm.h"
+
 typedef void (*op_fn)(lomg);
 
-void op_add(lomg);    // 1   R 0
-void op_sub(lomg);    // 1   R 1
-void op_mult(lomg);   // 1   R 2
-void op_addu(lomg);   // 1   R 3
-void op_subu(lomg);   // 1   R 4
-void op_multu(lomg);  // 1   R 5
-void op_sll(lomg);    // 1   R 6
-void op_srl(lomg);    // 1   R 7
-void op_sra(lomg);    // 1   R 8
+void op_add(lomg instruction);    // 1   R 0
+void op_sub(lomg instruction);    // 1   R 1
+void op_mult(lomg instruction);   // 1   R 2
+void op_addu(lomg instruction);   // 1   R 3
+void op_subu(lomg instruction);   // 1   R 4
+void op_multu(lomg instruction);  // 1   R 5
+void op_sll(lomg instruction);    // 1   R 6
+void op_srl(lomg instruction);    // 1   R 7
+void op_sra(lomg instruction);    // 1   R 8
 
-void op_and(lomg);    // 2   R 0
-void op_nand(lomg);   // 2   R 1
-void op_nor(lomg);    // 2   R 2
-void op_not(lomg);    // 2   R 3
-void op_or(lomg);     // 2   R 4
-void op_xnor(lomg);   // 2   R 5
-void op_xor(lomg);    // 2   R 6
+void op_and(lomg instruction);    // 2   R 0
+void op_nand(lomg instruction);   // 2   R 1
+void op_nor(lomg instruction);    // 2   R 2
+void op_not(lomg instruction);    // 2   R 3
+void op_or(lomg instruction);     // 2   R 4
+void op_xnor(lomg instruction);   // 2   R 5
+void op_xor(lomg instruction);    // 2   R 6
 
-void op_addi(lomg);   // 3   I
-void op_subi(lomg);   // 4   I
-void op_multi(lomg);  // 5   I
-void op_addui(lomg);  // 22  I
-void op_subui(lomg);  // 23  I
-void op_multui(lomg); // 24  I
-void op_slli(lomg);   // 34  I
-void op_srli(lomg);   // 35  I
-void op_srai(lomg);   // 36  I
+void op_addi(lomg instruction);   // 3   I
+void op_subi(lomg instruction);   // 4   I
+void op_multi(lomg instruction);  // 5   I
+void op_addui(lomg instruction);  // 22  I
+void op_subui(lomg instruction);  // 23  I
+void op_multui(lomg instruction); // 24  I
+void op_slli(lomg instruction);   // 34  I
+void op_srli(lomg instruction);   // 35  I
+void op_srai(lomg instruction);   // 36  I
 
-void op_andi(lomg);   // 6   I
-void op_nandi(lomg);  // 7   I
-void op_nori(lomg);   // 8   I
-void op_ori(lomg);    // 9   I
-void op_xnori(lomg);  // 10  I
-void op_xori(lomg);   // 11  I
+void op_andi(lomg instruction);   // 6   I
+void op_nandi(lomg instruction);  // 7   I
+void op_nori(lomg instruction);   // 8   I
+void op_ori(lomg instruction);    // 9   I
+void op_xnori(lomg instruction);  // 10  I
+void op_xori(lomg instruction);   // 11  I
 
-void op_lui(lomg);    // 13  I
-void op_sl(lomg);     // 14  R 0
-void op_sle(lomg);    // 14  R 1
-void op_seq(lomg);    // 14  R 2
-void op_slu(lomg);    // 14  R 3
-void op_sleu(lomg);   // 14  R 4
+void op_lui(lomg instruction);    // 13  I
+void op_sl(lomg instruction);     // 14  R 0
+void op_sle(lomg instruction);    // 14  R 1
+void op_seq(lomg instruction);    // 14  R 2
+void op_slu(lomg instruction);    // 14  R 3
+void op_sleu(lomg instruction);   // 14  R 4
 
-void op_sli(lomg);    // 25  I
-void op_slei(lomg);   // 26  I
-void op_seqi(lomg);   // 27  I
-void op_slui(lomg);   // 28  I
-void op_sleui(lomg);  // 29  I
+void op_sli(lomg instruction);    // 25  I
+void op_slei(lomg instruction);   // 26  I
+void op_seqi(lomg instruction);   // 27  I
+void op_slui(lomg instruction);   // 28  I
+void op_sleui(lomg instruction);  // 29  I
 
-void op_j(lomg);      // 15  J
-void op_jc(lomg);     // 16  J
-void op_jl(lomg);     // 32  J
-void op_jlc(lomg);    // 33  J
+void op_j(lomg instruction);      // 15  J
+void op_jc(lomg instruction);     // 16  J
+void op_jl(lomg instruction);     // 32  J
+void op_jlc(lomg instruction);    // 33  J
 
-void op_jr(lomg);     // 17  R 0
-void op_jrc(lomg);    // 17  R 1
-void op_jrl(lomg);    // 17  R 2
-void op_jrlc(lomg);   // 17  R 3
+void op_jr(lomg instruction);     // 17  R 0
+void op_jrc(lomg instruction);    // 17  R 1
+void op_jrl(lomg instruction);    // 17  R 2
+void op_jrlc(lomg instruction);   // 17  R 3
 
-void op_c(lomg);      // 18  R 0
-void op_l(lomg);      // 18  R 1
-void op_s(lomg);      // 18  R 2
-void op_cb(lomg);     // 18  R 3
-void op_lb(lomg);     // 18  R 4
-void op_sb(lomg);     // 18  R 5
+void op_c(lomg instruction);      // 18  R 0
+void op_l(lomg instruction);      // 18  R 1
+void op_s(lomg instruction);      // 18  R 2
+void op_cb(lomg instruction);     // 18  R 3
+void op_lb(lomg instruction);     // 18  R 4
+void op_sb(lomg instruction);     // 18  R 5
 
-void op_li(lomg);     // 19  I
-void op_si(lomg);     // 20  I
-void op_lbi(lomg);    // 37  I
-void op_sbi(lomg);    // 38  I
-void op_set(lomg);    // 21  I
+void op_li(lomg instruction);     // 19  I
+void op_si(lomg instruction);     // 20  I
+void op_lbi(lomg instruction);    // 37  I
+void op_sbi(lomg instruction);    // 38  I
+void op_set(lomg instruction);    // 21  I
 
 #endif
