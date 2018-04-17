b2185580eb12b7951e7ba714766c73b5fdfa5a6c
Kai Tamkun <ktamkun@ucsc.edu>
2018-04-17 14:04:53 -0700

    Slightly reduced code duplication in disassemble.c

--- wvm/c/disassemble.c
+++ wvm/c/disassemble.c
@@ -1,3 +1,8 @@
+/************************************************
+ * Contains functions for converting assembled  *
+ * instructions back into text representations. *
+ ************************************************/
+
 #include <stdio.h>
 #include "registers.h"
 #include "instruction.h"
@@ -244,14 +249,14 @@ void wvm_disassemble_i(char *str, word instruction) {
 			break;
 		case OP_SLI:
 		case OP_SLUI:
-			sprintf(str, "%s$%s%s %s<%s %s%d%s %s->%s %s$%s%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
+			wvm_disassemble_i_comp(str, rs, rd, imm, "<");
 			break;
 		case OP_SLEI:
 		case OP_SLEUI:
-			sprintf(str, "%s$%s%s %s<=%s %s%d%s %s->%s %s$%s%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
+			wvm_disassemble_i_comp(str, rs, rd, imm, "<=");
 			break;
 		case OP_SEQI:
-			sprintf(str, "%s$%s%s %s==%s %s%d%s %s->%s %s$%s%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
+			wvm_disassemble_i_comp(str, rs, rd, imm, "==");
 			break;
 		default:
 			sprintf(str, "%s(Unknown I-type)%s", ANSI_DIM, ANSI_RESET);
@@ -264,18 +269,22 @@ void wvm_disassemble_i(char *str, word instruction) {
 		sprintf(str, "%s /b", str);
 }
 
+void wvm_disassemble_i_comp(char *str, reg_t rs, reg_t rd, imm_t imm, char *oper) {
+	sprintf(str, "%s$%s%s %s%s%s %s%d%s %s->%s %s$%s%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, oper, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
+}
+
 void wvm_disassemble_i_alt_op(char *str, reg_t rs, reg_t rd, imm_t imm, char *oper) {
 	sprintf(str, "%s$%s%s %s%s%s%s %s%d%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, oper, rs == rd? "=" : "", ANSI_RESET, COLOR_IMM, imm, ANSI_RESET);
 	if (rs != rd)
 		sprintf(str, "%s %s->%s %s$%s%s", str, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
 }
 
-void wvm_disassemble_i_math(char *str, reg_t rs, reg_t rd, imm_t imm, char op) {
+void wvm_disassemble_i_math(char *str, reg_t rs, reg_t rd, imm_t imm, char oper) {
 	if (rs == rd) {
 		if (imm == 1)
-			sprintf(str, "%s$%s%s%c%c%s", COLOR_REG, wvm_decode_reg(rs), ANSI_DIM, op, op, ANSI_RESET);
-		else sprintf(str, "%s$%s%s %s%c=%s %s%d%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, op, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET);
-	} else sprintf(str, "%s$%s%s %s%c%s %s%d%s %s->%s %s$%s%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, op, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
+			sprintf(str, "%s$%s%s%c%c%s", COLOR_REG, wvm_decode_reg(rs), ANSI_DIM, oper, oper, ANSI_RESET);
+		else sprintf(str, "%s$%s%s %s%c=%s %s%d%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, oper, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET);
+	} else sprintf(str, "%s$%s%s %s%c%s %s%d%s %s->%s %s$%s%s", COLOR_REG, wvm_decode_reg(rs), ANSI_RESET, ANSI_BOLD, oper, ANSI_RESET, COLOR_IMM, imm, ANSI_RESET, ANSI_DIM, ANSI_RESET, COLOR_REG, wvm_decode_reg(rd), ANSI_RESET);
 }
 
 void wvm_disassemble_j(char *str, word instruction) {
