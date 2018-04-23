aa493ce85d808da3a5eec7db6d8ec0910226fd92
Kai Tamkun <ktamkun@ucsc.edu>
2018-04-22 23:47:39 -0700

    Added a warning when <2 inputs are specified.

--- wasm/wld.js
+++ wasm/wld.js
@@ -31,19 +31,30 @@ class Linker {
 	}
 
 	link() {
+		if (this.objectFilenames.length < 2) {
+			console.error(`${chalk.yellow("?")} Multiple input files are needed.`);
+			process.exit(1);
+		}
+
+		// Step 1
 		this.parser = new Parser();
 		this.parser.open(this.objectFilenames[0]);
 
 		const {raw, parsed} = this.parser;
+		
+		// Step 2
 		const codeLength = this.parser.getCodeLength();
 		const dataLength = this.parser.getDataLength();
 		const mainSymbols = this.parser.getSymbols();
 		this.symbolTable = _.cloneDeep(mainSymbols);
 		
+		// Step 3
 		this.desymbolize(this.parser.rawCode, mainSymbols);
 
-		// fs.writeFileSync(this.options.out, WASMC.longs2strs(this.finalizeOutput()).join("\n"));
-		// console.log(`${chalk.green("\u2714")} Successfully linked ${chalk.bold(this.filename)} and saved the output to ${chalk.bold(this.options.out)}.`);
+		// Steps 4–6
+		let extraSymbolLength = this.symbolTable.length;
+		let extraCodeLength = codeLength;
+		let extraDataLength = dataLength;
 	}
 
 	/**
@@ -108,6 +119,24 @@ class Linker {
 		}
 
 		return null;
+	}
+
+	/**
+	 * Writes the output of the linking process to a file.
+	 * @param {Long[]} longs The final linked output as an array of Longs.
+	 * @param {string} [outfile] A filename (`options.out` if not specified).
+	 */
+	writeOutput(longs, outfile=this.options.out) {
+		fs.writeFileSync(outfile, WASMC.longs2strs(longs).join("\n"));
+	}
+
+	/**
+	 * Prints a message to the console that indicates the linking was successful.
+	 * @param {string} infile The input filename.
+	 * @param {string} outfile The output filename.
+	 */
+	printSuccess(infile, outfile) {
+		console.log(`${chalk.green("\u2714")} Successfully linked ${chalk.bold(infile)} and saved the output to ${chalk.bold(outfile)}.`);
 	}
 
 	warn(...args) {
