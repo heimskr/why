30372e1f2619450858e76183a05accb71211b35c
Kai Tamkun <ktamkun@ucsc.edu>
2018-12-19 23:56:25 -0800

    Filled in interrupts.c/.h

--- wvm/c/interrupts.c
+++ wvm/c/interrupts.c
@@ -1,1 +1,68 @@
 #include "interrupts.h"
+#include <stdio.h>
+
+#define TOFROM(id) case INT_##id: to = INT_##id##_TO; from = INT_##id##_FROM; break
+
+uword inttab = 0;
+
+/**
+ * Performs an interrupt.
+ * @param id The ID of the interrupt.
+ * @return Whether to increment the counter after performing the interrupt. (Useful only for software interrupts.)
+ */
+int wvm_interrupt(imm_t id) {
+	if (INTERRUPT_MAX < id) {
+		fprintf(stderr, "Unknown interrupt: %u\n", id);
+		exit(1);
+	}
+
+	if (inttab == 0) {
+#ifdef INTERRUPTS_DEBUG
+		fprintf(stderr, "Warning: no interrupt table registered; ignoring interrupt.\n");
+#endif
+		return 1;
+	}
+
+	if (id == INT_NULL) {
+		return 1;
+	}
+
+	ring_t to, from;
+	switch (id) {
+		TOFROM(SYSTEM);
+		TOFROM(TIMER);
+		TOFROM(PROTEC);
+	}
+
+	if (from < cur_ring) {
+#ifdef INTERRUPTS_DEBUG
+		fprintf(stderr, "Insufficient privilege for interrupt %u (currently %d, required %d); ignoring interrupt.\n",
+			id, cur_ring, from);
+#endif
+		return 1;
+	}
+
+	registers[R_E0] = pc + 8;
+	registers[R_E1] = cur_ring;
+
+	if (-1 < to) {
+		cur_ring = to;
+	}
+
+	wvm_jump(inttab + 8 * id);
+	return 0;
+}
+
+void int_protec() {
+	wvm_interrupt(INT_PROTEC);
+}
+
+int check_kernel() {
+	if (RING_KERNEL < cur_ring) {
+		int_protec();
+		return 0;
+	}
+
+	return 1;
+}
+
--- wvm/c/interrupts.h
+++ wvm/c/interrupts.h
@@ -1,6 +1,31 @@
 #ifndef _INTERRUPTS_H_
 #define _INTERRUPTS_H_
 
-void wvm_interrupt();
+#include "instruction.h"
+#include "wvm.h"
+
+uword inttab;
+
+int wvm_interrupt(imm_t id);
+void int_protec();
+int check_kernel();
+
+#define INTERRUPT_MAX	3
+
+#define INT_NULL		0
+#define INT_NULL_FROM	-1
+#define INT_NULL_TO		-1
+
+#define INT_SYSTEM		1
+#define INT_SYSTEM_FROM	0
+#define INT_SYSTEM_TO	-1
+
+#define INT_TIMER		2
+#define INT_TIMER_FROM	-1
+#define INT_TIMER_TO	0
+
+#define INT_PROTEC		3
+#define INT_PROTEC_FROM	0
+#define INT_PROTEC_TO	2
 
 #endif
