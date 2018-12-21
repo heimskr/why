8a1f5eb2613c216ca1ee3bbf8c6504612e85e91a
Kai Tamkun <ktamkun@ucsc.edu>
2018-12-21 08:42:29 -0800

    read/assigned warnings now include the name of the function

--- llvm/ll2w.js
+++ llvm/ll2w.js
@@ -484,13 +484,14 @@ class LL2W {
 			});
 
 			if (range[0] == null && range[1] != null) {
-				console.warn(WARN, "Variable", chalk.bold("%" + v), "is read but never assigned.");
-			} else if (range[0] != null && range[1] == null) {
+				console.warn(WARN, "Variable", chalk.bold("%" + v), "is read but never assigned in",
+					chalk.bold(fn.meta.name) + "."); } else if (range[0] != null && range[1] == null) {
 				// Sometimes, the return value of a call is stored in a variable that isn't ever read.
 				range[1] = range[0];
 				if (numArgs <= range[0] && !(assigners[v] instanceof Array && assigners[v][1] == "call")) {
 					// Don't warn if the function arguments aren't read. The compiler has already complained about it.
-					console.warn(WARN, "Variable", chalk.bold("%" + v), "is assigned but never read.", assigners[v]);
+					console.warn(WARN, "Variable", chalk.bold("%" + v), "is assigned but never read in",
+						chalk.bold(fn.meta.name) + ".", assigners[v]);
 				}
 			}
 		}
