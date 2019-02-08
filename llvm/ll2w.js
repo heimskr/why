ccdf31c06aaf0484ccae4686ec145e925611bbc1
Kai Tamkun <ktamkun@ucsc.edu>
2019-02-08 04:19:29 -0800

    Merge set computation was moved to graph.js.

--- llvm/ll2w.js
+++ llvm/ll2w.js
@@ -645,13 +645,6 @@ class LL2W {
 		// return {liveIn, liveOut};
 	}
 
-	static computeMergeSet(cfg) {
-		// "A Practical and Fast Iterative Algorithm for φ-Function Computation Using DJ Graphs"
-		// Das and Ramakrishna (2005)
-		const dj = cfg.djGraph(0);
-		let reqPass = false;
-	}
-
 	computeLiveRanges(fn) {
 		// Is this actually how it's done? In some examples, it's done backwards.
 		// I'd imagine live variable analysis is less complicated in SSA because variables are assigned once,
