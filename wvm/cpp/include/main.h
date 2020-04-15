bbd900f93a6fd7118a89c6829f80163120284ee7
Kai Tamkun <ktamkun@ucsc.edu>
2020-04-14 22:38:55 -0700

    Expand skeleton

---
+++ wvm/cpp/include/main.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "lib/formicine/ansi.h"
+#include "haunted/core/terminal.h"
+
+namespace WVM {
+	haunted::terminal term;
+}
--- wvm/cpp/src/main.cpp
+++ wvm/cpp/src/main.cpp
@@ -2,5 +2,5 @@
 #include "ansi.h"
 
 int main() {
-
+	
 }
