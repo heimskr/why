4f3d31e485e069b99e4bfb8469ecb6fb6e0c9f6c
Kai Tamkun <ktamkun@ucsc.edu>
2020-04-14 23:05:07 -0700

    Committing what little I have before I try a server-client thing

--- wvm/cpp/include/main.h
+++ wvm/cpp/include/main.h
@@ -2,7 +2,22 @@
 
 #include "lib/formicine/ansi.h"
 #include "haunted/core/terminal.h"
+#include "haunted/ui/boxes/expandobox.h"
 
 namespace WVM {
-	haunted::terminal term;
+	class Main {
+		private:
+			haunted::terminal terminal;
+			haunted::ui::boxes::expandobox *expando;
+			haunted::ui::textbox *memory, *registers
+
+			void initExpando();
+
+		public:
+			Main() = default;
+			Main(Main &&) = delete;
+			Main(const Main &) = delete;
+
+			void start();
+	};
 }
--- wvm/cpp/src/main.cpp
+++ wvm/cpp/src/main.cpp
@@ -2,5 +2,21 @@
 #include "ansi.h"
 
 int main() {
-	
+	WVM::Main main;
+	main.start();
+}
+
+namespace WVM {
+	void Main::start() {
+		DBG("Hello, World!");
+		terminal.watch_size();
+		terminal.start_input();
+	}
+
+	void Main::initExpando() {
+		expando = new haunted::ui::boxes::expandobox(&terminal, terminal.get_position(),
+			haunted::ui::boxes::box_orientation::horizontal);
+		expando->set_name("expando");
+		terminal.set_root(expando);
+	}
 }
