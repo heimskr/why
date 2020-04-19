ed73eb0e22274ab17943b715b16971b3c8e6b255
Kai Tamkun <ktamkun@ucsc.edu>
2020-04-18 21:47:35 -0700

    Add RMap, IMap and JMap

--- wvm/cpp/include/Operations.h
+++ wvm/cpp/include/Operations.h
@@ -1,6 +1,8 @@
 #ifndef WVM_OPERATIONS_H_
 #define WVM_OPERATIONS_H_
 
+#include <set>
+
 #include "Defs.h"
 
 namespace WVM::Operations {
@@ -8,6 +10,168 @@ namespace WVM::Operations {
 	void decodeRType(int &opcode, Word *&rs, Word *&rt, Word *&rd, Conditions &, int &flags, int &funct);
 	void decodeIType(int &opcode, Word *&rs, Word *&rd,  Conditions &, int &flags, HWord &immediate);
 	void decodeJType(int &opcode, Word *&rs, bool &link, Conditions &, int &flags, HWord &address);
+
+	extern std::set<int> RSet;
+	extern std::set<int> ISet;
+	extern std::set<int> JSet;
+
+#define OP_NOP 0
+
+#define OP_ADD 1
+#define FN_ADD 0
+#define OP_SUB 1
+#define FN_SUB 1
+#define OP_MULT 1
+#define FN_MULT 2
+#define OP_MULTU 1
+#define FN_MULTU 5
+#define OP_SLL 1
+#define FN_SLL 6
+#define OP_SRL 1
+#define FN_SRL 7
+#define OP_SRA 1
+#define FN_SRA 8
+#define OP_MOD 1
+#define FN_OP 9
+#define OP_DIV 1
+#define FN_DIV 10
+#define OP_DIVU 1
+#define FN_DIVU 11
+
+#define OP_AND 2
+#define FN_AND 0
+#define OP_NAND 2
+#define FN_NAND 1
+#define OP_NOR 2
+#define FN_NOR 2
+#define OP_NOT 2
+#define FN_NOT 3
+#define OP_OR 2
+#define FN_OR 4
+#define OP_XNOR 2
+#define FN_XNOR 5
+#define OP_XOR 2
+#define FN_XOR 6
+#define OP_LAND 2
+#define FN_LAND 8
+#define OP_LNAND 2
+#define FN_LNAND 9
+#define OP_LNOR 2
+#define FN_LNOR 10
+#define OP_LNOT 2
+#define FN_LNOT 11
+#define OP_LOR 2
+#define FN_LOR 12
+#define OP_LXNOR 2
+#define FN_LXNOR 13
+#define OP_LXOR 2
+#define FN_LXOR 14
+
+#define OP_ADDI 3
+#define OP_SUBI 4
+#define OP_MULTI 5
+
+#define OP_ANDI 6
+#define OP_NANDI 7
+#define OP_NORI 8
+#define OP_ORI 9
+#define OP_XNORI 10
+#define OP_XORI 11
+
+#define OP_LUI 13
+
+#define OP_SL 14
+#define FN_SL 0
+#define OP_SLE 14
+#define FN_SLE 1
+#define OP_SEQ 14
+#define FN_SEQ 2
+#define OP_SLU 14
+#define FN_SLU 3
+#define OP_SLEU 14
+#define FN_SLEU 4
+
+#define OP_J 15
+#define OP_JC 16
+
+#define OP_JR 17
+#define FN_JR 0
+#define OP_JRC 17
+#define FN_JRC 1
+#define OP_JRL 17
+#define FN_JRL 2
+#define OP_JRLC 17
+#define FN_JRLC 3
+
+#define OP_C 18
+#define FN_C 0
+#define OP_L 18
+#define FN_L 1
+#define OP_S 18
+#define FN_S 2
+#define OP_CB 18
+#define FN_CB 3
+#define OP_LB 18
+#define FN_LB 4
+#define OP_SB 18
+#define FN_SB 5
+#define OP_SPUSH 18
+#define FN_SPUSH 6
+#define OP_SPOP 18
+#define FN_SPOP 7
+#define OP_CH 18
+#define FN_CH 8
+#define OP_LH 18
+#define FN_LH 9
+#define OP_SH 18
+#define FN_SH 10
+
+#define OP_LI 19
+#define OP_SI 20
+#define OP_SET 21
+
+#define OP_MULTUI 24
+#define OP_SLI 25
+#define OP_SLEI 26
+#define OP_SEQI 27
+#define OP_SLUI 28
+#define OP_SLEUI 29
+#define OP_MODI 30
+
+#define OP_PR 31
+#define FN_PR 1
+#define OP_HALT 31
+#define FN_HALT 2
+#define OP_EVAL 31
+#define FN_EVAL 3
+#define OP_PRC 31
+#define FN_PRC 4
+#define OP_PRD 31
+#define FN_PRD 5
+#define OP_PRX 31
+#define FN_PRX 6
+#define OP_SLEEP 31
+#define FN_SLEEP 7
+
+#define OP_INT 32
+#define OP_RIT 33
+#define OP_SLLI 34
+#define OP_SRLI 35
+#define OP_SRAI 36
+#define OP_LBI 37
+#define OP_SBI 38
+#define OP_LNI 39
+#define OP_LBNI 40
+#define OP_SGI 41
+#define OP_SGEI 42
+#define OP_TIME 48
+#define OP_TIMEI 49
+#define OP_RING 50
+#define OP_RINGI 51
+#define OP_DIVI 52
+#define OP_DIVUI 53
+#define OP_DIVII 54
+#define OP_DIVUII 55
 
 	void nopOp(VM &, Word instruction);    // 0   X
 
