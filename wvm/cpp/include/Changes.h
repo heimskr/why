bb4859c41dc5737639331b639aa417e9fb801718
Kai Tamkun <ktamkun@ucsc.edu>
2020-05-08 16:06:15 -0700

    Initial work on undo

---
+++ wvm/cpp/include/Changes.h
@@ -0,0 +1,25 @@
+#ifndef WVM_CHANGES_H_
+#define WVM_CHANGES_H_
+
+#include "Defs.h"
+
+namespace WVM {
+	class VM;
+
+	struct Change {
+		virtual void apply(VM &, bool strict = false) = 0;
+		virtual void undo(VM &, bool strict = false)  = 0;
+	};
+
+	struct MemoryChange: public Change {
+		Word address, from, to;
+		Size size;
+		MemoryChange(Word address_, Word from_, Word to_, Size size_):
+			address(address_), from(from_), to(to_), size(size_) {}
+
+		void apply(VM &, bool strict = false) override;
+		void undo(VM &, bool strict = false) override;
+	};
+}
+
+#endif
---
+++ wvm/cpp/src/Changes.cpp
@@ -0,0 +1,17 @@
+#include "Changes.h"
+#include "VM.h"
+#include "VMError.h"
+
+namespace WVM {
+	void MemoryChange::apply(VM &vm, bool strict) {
+		if (strict && vm.get(address, size) != from)
+			throw VMError("Unable to apply MemoryChange: memory at address isn't the expected from-value");
+		vm.set(address, to, size);
+	}
+
+	void MemoryChange::undo(VM &vm, bool strict) {
+		if (strict && vm.get(address, size) != to)
+			throw VMError("Unable to undo MemoryChange: memory at address isn't the expected to-value");
+		vm.set(address, from, size);
+	}
+}
