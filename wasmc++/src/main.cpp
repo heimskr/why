55a413cd67c7826c653b1c075893a21372132f4c
Kai Tamkun <ktamkun@ucsc.edu>
2021-08-17 20:40:05 -0700

    Take both an input argument and an output argument

--- wasmc++/src/main.cpp
+++ wasmc++/src/main.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 
 #include "parser/Parser.h"
+#include "util/Util.h"
 #include "wasm/Assembler.h"
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		std::cerr << "Usage: " << argv[0] << " <filename>\n";
+	if (argc != 3) {
+		std::cerr << "Usage: " << argv[0] << " <input> <output>\n";
 		return 1;
 	}
 
@@ -18,6 +19,7 @@ int main(int argc, char **argv) {
 	text.reserve(file.tellg());
 	file.seekg(0, std::ios::beg);
 	text.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	file.close();
 
 	auto &parser = Wasmc::wasmParser;
 	parser.in(text);
@@ -25,7 +27,21 @@ int main(int argc, char **argv) {
 	parser.parse();
 
 	Wasmc::Assembler assembler(parser);
-	std::cout << assembler.assemble() << "\n";
+
+	std::ofstream outfile(argv[2]);
+	if (!outfile.is_open())
+		throw std::runtime_error("Couldn't open file for writing");
+	try {
+		outfile << assembler.assemble();
+	} catch (const std::exception &err) {
+		Wasmc::error() << "Compilation failed: " << err.what() << "\n";
+		outfile.close();
+		return 2;
+	}
+
+	outfile.close();
+	Wasmc::success() << "Assembled \e[1m" << argv[1] << "\e[22m and saved the results to \e[1m" << argv[2]
+		<< "\e[22m.\n";
 
 	return 0;
 }
