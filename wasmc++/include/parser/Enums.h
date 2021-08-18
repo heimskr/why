c69b1988465bb15917c23a1904e26c7611de04b5
Kai Tamkun <ktamkun@ucsc.edu>
2021-08-17 20:40:28 -0700

    Fix broken conditions

--- wasmc++/include/parser/Enums.h
+++ wasmc++/include/parser/Enums.h
@@ -4,7 +4,7 @@
 #include <unordered_map>
 
 namespace Wasmc {
-	enum class Condition {Positive, Negative, Zero, Nonzero, None};
+	enum class Condition {Positive = 0b1000, Negative = 0b1001, Zero = 0b1010, Nonzero = 0b1011, None = 0b0000};
 	enum class PrintType {Dec, Bin, Hex, Char, Full};
 	enum class QueryType {Memory};
 
